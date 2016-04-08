2728e880843c4eac8f4996985ee43e560a3ce92f
Raik Otto <raik.otto@hu-berlin.de>
2016-04-08 09:17:10 +0000

    update testsuite
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/Uniquorn@115992 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_identification.R
+++ inst/unitTests/test_identification.R
@@ -1,11 +1,14 @@
-library("Uniquorn")
-library("testthat")
+test_identification = function(){
+  
+  set.seed(1)
 
-HT29_vcf_file = system.file("extdata/HT29.vcf.gz", package="Uniquorn")
+  HT29_vcf_file = system.file("extdata/HT29.vcf.gz", package="Uniquorn")
+  
+  ident_result = identify_vcf_file( HT29_vcf_file, ref_gen = "GRCH37" )
+  
+  checkTrue( class(ident_result) == "data.frame" )
+  
+  checkTrue( dim(ident_result)[1] == 60 )
+  checkTrue( dim(ident_result)[2] == 9  )
 
-ident_result = identify_vcf_file( HT29_vcf_file, ref_gen = "GRCH37" )
-
-expect_that( ident_result, is_a("data.frame") )
-
-expect_that( dim(ident_result)[1], equals( 60 )  )
-expect_that( dim(ident_result)[2], equals( 9  ) )
+}
