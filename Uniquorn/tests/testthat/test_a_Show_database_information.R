ba9692a2d8d0596e3c29ebf56ad77029a560d170
Jan-Niklas Rössler <janniklas@Jan-Niklass-MacBook-Pro.local>
2018-01-11 17:18:44 +0100

    message

--- Uniquorn/tests/testthat/test_a_Show_database_information.R
+++ Uniquorn/tests/testthat/test_a_Show_database_information.R
@@ -16,38 +16,43 @@ test_that("show_contained_ccls returns correct object", {
 test_that("show_contained_variants_in_library returns correct object", {
     names = read_library_names(ref_gen = "GRCH37")
     if("CELLMINER" %in% names){
+        
         g_ranges = show_contained_variants_in_library(
             library_name = "CELLMINER"
         )
-  
+        
         expect_is(g_ranges, "GRanges")
-        expect_equal(length(g_ranges), 57262)
-        expect_equal(sum(grepl("CELLMINER", g_ranges$Member_CCLs)), 57262)
+        expect_true(length(g_ranges) > 50000)
+        expect_equal(sum(grepl("CELLMINER", g_ranges$Member_CCLs)), length(g_ranges))
     }
 })
 
 test_that("show_contained_variants_for_ccl returns the right object", {
     names = read_library_names(ref_gen = "GRCH37")
+    ccls_all = show_contained_ccls()
     if("CELLMINER" %in% names){
+      
+        index = ccls_all$Library == "CELLMINER"
+        ccls_all = ccls_all[index,]
+        
+        ccl_index = sample(1:length(ccls_all), 1, replace = FALSE)
+        ccl = ccls_all$CCL[ccl_index]
+        
         variants = show_contained_variants_for_ccl(
-            name_ccl = "786_0", library_name = "CELLMINER"
+            name_ccl = ccl, library_name = "CELLMINER"
         )
   
         expect_is(variants, "GRanges")
-        expect_equal(length(variants), 584)
+        expect_true(length(variants) > 1)
+        
+        ccl_index = sample(1:length(ccls_all), 1, replace = FALSE)
+        ccl = ccls_all$CCL[ccl_index]
+        
+        variants = show_contained_variants_for_ccl(
+          name_ccl = ccl, library_name = "CELLMINER"
+        )
+        
+        expect_is(variants, "GRanges")
+        expect_true(length(variants) > 1)
   }
-})
-
-test_that("show_which_ccls_contain_variant returns the right object", {
-    names = read_library_names(ref_gen = "GRCH37")
-    if("CELLMINER" %in% names){
-        ccls = show_which_ccls_contain_variant(start = 92030762,
-            end = 92030762, chromosome = 8, library_name = "CELLMINER")
-        names = unlist(strsplit(ccls$Member_CCLs, ",", fixed = TRUE))
-  
-        expect_is(ccls, "GRanges")
-        expect_equal(length(names), 8)
-        expect_true(any(grepl("786_0_CELLMINER", names, fixed = TRUE)))
-        expect_true(any(grepl("PC_3_CELLMINER", names, fixed = TRUE)))
-    }
 })
