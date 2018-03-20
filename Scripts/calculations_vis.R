e994b9400d28bd278a7ce3a2e2182256a8238ece
RaikOtto <fubioinf@gmail.com>
2018-03-20 12:46:25 +0100

    update

--- Scripts/calculations_vis.R
+++ Scripts/calculations_vis.R
@@ -122,38 +122,92 @@ ccl_mat = data.frame(
 
 ### Per_ccl_success
 
-avg_file   = read.table("~/Uniquorn_data/benchmark_vcf_files/0_5_Benchmark_identification_result.tab", sep = "\t", header = T)
+avg_file = read.table("~/Uniquorn_data/benchmark_vcf_files/0_5_Benchmark_identification_result.tab", sep = "\t", header = T, stringsAsFactors = F)
 #c = show_contained_variants_for_ccl("105KC", library_name = "EGA", mutational_weight_inclusion_threshold = 0.5)
 
 ref_ccls_cellminer = show_contained_variants_in_library( library_name = "CELLMINER", mutational_weight_inclusion_threshold = 0.5)$Member_CCLs
+ref_ccls_cellminer = as.character(unlist( str_split( ref_ccls_cellminer, pattern = "," )))
+ref_ccls_cellminer = str_replace( ref_ccls_cellminer, pattern = "_CELLMINER", "")
+ref_ccls_cellminer = str_replace_all( ref_ccls_cellminer, pattern = "_", "")
+ref_ccls_cellminer = as.character(unlist( str_split( ref_ccls_cellminer, pattern = "," )))
+ref_ccls_cellminer = paste(ref_ccls_cellminer, "CELLMINER", sep = "_")
+
 ref_ccls_cosmic = show_contained_variants_in_library( library_name = "COSMIC", mutational_weight_inclusion_threshold = 0.5)$Member_CCLs
 ref_ccls_cosmic = str_to_upper( ref_ccls_cosmic )
-ref_ccls_cosmic = str_replace_all( ref_ccls_cosmic, pattern = "-", "_")
-ref_ccls_cosmic = str_replace_all( ref_ccls_cosmic, pattern = "\\.", "_")
-#ref_ccls_cosmic = paste(ref_ccls_cosmic, "GDC", sep = "_")
-ref_ccls_ccle = show_contained_variants_in_library( library_name = "CCLE", mutational_weight_inclusion_threshold = 0.5)$Member_CCLs
-ref_ccls_ega = show_contained_variants_in_library( library_name = "EGA", mutational_weight_inclusion_threshold = 0.5)$Member_CCLs
-ref_ccls_gdc = show_contained_variants_in_library( library_name = "GDC", mutational_weight_inclusion_threshold = 0.5)$Member_CCLs
+ref_ccls_cosmic = str_replace_all( ref_ccls_cosmic, pattern = "-", "")
+ref_ccls_cosmic = str_replace_all( ref_ccls_cosmic, pattern = "\\.", "")
+ref_ccls_cosmic = as.character(unlist( str_split( ref_ccls_cosmic, pattern = "," )))
+ref_ccls_cosmic = paste(ref_ccls_cosmic, "GDC", sep = "_")
 
-all_ccls = unique( as.character(unlist( str_split( ref_ccls, pattern = "," ))))
+ref_ccls_ccle = show_contained_variants_in_library( library_name = "CCLE", mutational_weight_inclusion_threshold = 0.5)$Member_CCLs
+ref_ccls_ccle = as.character(unlist( str_split( ref_ccls_ccle, pattern = "," )))
+ref_ccls_ccle = paste(ref_ccls_ccle, "CCLE", sep = "_")
+
+ref_ccls_ega = show_contained_variants_in_library( library_name = "EGA", mutational_weight_inclusion_threshold = 0.5)$Member_CCLs
+ref_ccls_ega = str_replace_all( ref_ccls_ega, pattern = "\\.", "")
+ref_ccls_ega = str_replace_all( ref_ccls_ega, pattern = "-", "")
+ref_ccls_ega = as.character(unlist( str_split( ref_ccls_ega, pattern = "," )))
+
+ref_ccls_gdc = show_contained_variants_in_library( library_name = "GDC", mutational_weight_inclusion_threshold = 0.5)$Member_CCLs
+ref_ccls_gdc = str_replace_all( ref_ccls_gdc, pattern = "\\.", "")
+ref_ccls_gdc = str_replace_all( ref_ccls_gdc, pattern = "-", "")
+ref_ccls_gdc = as.character(unlist( str_split( ref_ccls_gdc, pattern = "," )))
 
 ref_counts <<- data.frame(
   "CCL" = as.character(),
   "Count" = as.character(),
-  "Library" = as.character()
+  "Library" = as.character(),
+  "Sensitivity" = as.character(),
+  "F1" = as.character(),
+  "PPV" = as.character()
 )
 
-count_per_ccl = sapply( all_ccls, FUN= function(ref_ccl){
+all_ccls = c(ref_ccls_cellminer,ref_ccls_cosmic,ref_ccls_ccle,ref_ccls_ega,ref_ccls_gdc)
+
+all_ccls = ref_ccls_cosmic
+all_ccls_uni = unique( as.character(unlist( str_split( all_ccls, pattern = "," ))))
+
+i <<- 0
+
+count_per_ccl = sapply( all_ccls_uni, FUN= function(ref_ccl){
     
     ref_ccl = as.character(ref_ccl)
-    count = as.character( sum ( str_detect( ref_ccls$Member_CCLs, pattern = ref_ccl ) ))
+    count = as.character( sum ( str_detect( all_ccls, pattern = ref_ccl ) ))
     library = as.character( tail( as.character( unlist( str_split(ref_ccl, pattern = "_") ) ), 1 ) )
+    
+    i <<- i + 1
+    print(i)
+    
+    perf_t = avg_file[ match( ref_ccl, avg_file$Query, nomatch = 0), ]
+    expected = as.character( unlist( str_split( perf_t$Expected, pattern = ",") ) )
+    expected = expected[expected != ""]
+    expected = length(expected)
+    tp = as.character( unlist( str_split( perf_t$True_positive, pattern = ",") ) )
+    tp = tp[tp != ""]
+    tp = length(tp)
+    fp = as.character( unlist( str_split( perf_t$False_positive, pattern = ",") ) )
+    fp = fp[fp != ""]
+    fp = length(fp)
+    fn = as.character( unlist( str_split( perf_t$False_negative, pattern = ",") ) )
+    fn = fn[fn != ""]
+    fn = length(fn)
+    Sensitivity = round( tp / expected * 100, 0 )
+    Specificity = round( tp / (tp + fp) * 100, 0 )
+    F1 = round( 2 * (Sensitivity * Specificity) / (Sensitivity + Specificity), 0)
+    PPV = round( tp / (tp + fp)  * 100,0)
+    
+    ref_ccl = str_replace( ref_ccl, pattern = paste("",library, sep = "_"),""  )
     
     ref_counts <<- data.frame(
         "CCL" = c(  as.character( ref_counts$CCL) , ref_ccl ),
         "Count" = c( as.character( ref_counts$Count), count),
-        "Library" = c( as.character( ref_counts$Library), library)
+        "Library" = c( as.character( ref_counts$Library), library),
+        "Sensitivity" = c( as.character( ref_counts$Sensitivity), Sensitivity),
+        "F1" = c( as.character( ref_counts$F1), F1),
+        "PPV" = c( as.character( ref_counts$PPV), PPV)
     )
   }
 )
-ref_counts$Library[ref_counts$Library == "COSMIC" ] = "CGP"
+ref_counts[1:5,]
+
+write.table(ref_counts,"~/U",sep ="\t", quote = F)
